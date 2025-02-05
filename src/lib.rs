f24adbc03af83b93b850c58269e074b603154703
reshane <shanekenny023@gmail.com>
2025-02-04 19:01:39 -0500

    Add Wbmp to ImageFormat enum

--- src/lib.rs
+++ src/lib.rs
@@ -221,6 +221,7 @@ pub mod flat;
 /// | TGA      | Yes                                       | Yes                                     |
 /// | TIFF     | Yes                                       | Yes                                     |
 /// | WebP     | Yes                                       | Yes (lossless only)                     |
+/// | Wbmp     | Yes                                       | Yes                                     |
 ///
 /// - \* Requires the `avif-native` feature, uses the libdav1d C library.
 ///
